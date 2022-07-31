cd8236d3b524853a0902422fe9e724b1043f5671
ashleyjensen <ajen147@wgu.edu>
2022-07-30 21:20:56 -0500

    UI.MainActivity Cleanup

--- app/src/main/java/com/ajensen/studentscheduleapp/UI/MainActivity.java
+++ app/src/main/java/com/ajensen/studentscheduleapp/UI/MainActivity.java
@@ -1,27 +1,17 @@
 package com.ajensen.studentscheduleapp.UI;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import com.ajensen.studentscheduleapp.Database.Repository;
-import com.ajensen.studentscheduleapp.Entity.Assessment;
-import com.ajensen.studentscheduleapp.Entity.Course;
-import com.ajensen.studentscheduleapp.Entity.Term;
-import com.ajensen.studentscheduleapp.R;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Date;
+import com.ajensen.studentscheduleapp.Database.Repository;
+import com.ajensen.studentscheduleapp.R;
 
 public class MainActivity extends AppCompatActivity {
     public static int numAlert;
-
-    // Delete
-    private static final String TAG = "MainActivity";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,32 +23,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, TermList.class);
         startActivity(intent);
         Repository repo = new Repository(getApplication());
-
-        // ** DELETE ** //
-
-        /*
-        // Add Terms
-        Term term = new Term(1, "Spring 2022", new Date(), new Date());
-        Log.i(TAG, "Term: " + term);
-        repo.insert(term);
-
-        term = new Term(2, "Fall 2022", new Date(), new Date());
-        repo.insert(term);
-
-
-        // Add Course
-        Course course = new Course(1, "Mobile App Dev", new Date(), new Date(), "Dropped", "Mr. Person",
-                "801-555-5555", "email@email.com", 1, "This is the first class! Yay!");
-        repo.insert(course);
-
-        course = new Course(2, "Software 1", new Date(), new Date(), "Plan to Take", "Mr. Person",
-                "801-555-5555", "email@email.com", 1, "This is the first class! Yay!");
-        repo.insert(course);
-
-        // Add Assessment
-        Assessment assessment = new Assessment(1, "First Test", "Pop Quiz", new Date(), new Date(), 1);
-        repo.insert(assessment);
-
-         */
     }
 }
