d964a88608ff234a9a6849625fa49ebf81061db5
ashleyjensen <ajen147@wgu.edu>
2022-07-30 22:33:25 -0500

    UI.TermList Cleanup

--- app/src/main/java/com/ajensen/studentscheduleapp/UI/TermList.java
+++ app/src/main/java/com/ajensen/studentscheduleapp/UI/TermList.java
@@ -1,14 +1,14 @@
 package com.ajensen.studentscheduleapp.UI;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ajensen.studentscheduleapp.Database.Repository;
 import com.ajensen.studentscheduleapp.Entity.Term;
@@ -45,8 +45,8 @@ public class TermList extends AppCompatActivity {
         adapter.setTermsList(termsList);
     }
 
+    // Inflate the menu; this adds items to the action bar if it is present.
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_term_list, menu);
         return true;
     }
